6eda3a6a19d0f8dda8cf6690e1e8b9b5c3572b39
hardinm78 <michaelhardin@michaels-imac.local>
2016-05-04 14:55:30 -0500

    Initial Commit

---
+++ Crazy_Stupid_Adventurer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Crazy_Stupid_Adventurer
+//
+//  Created by Michael Hardin on 5/4/16.
+//  Copyright © 2016 Michael Hardin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
